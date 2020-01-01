ee3163c50afdc1930c4007196bd5b99a1eba3148
ajithvgiri <ajithvgiri@qkopy.com>
2020-01-01 10:52:14 +0530

    gradle upgrade and demo app migrated to Kotlin

---
+++ app/src/main/java/com/ajithvgiri/searchdialog/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.ajithvgiri.searchdialog
+
+import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var searchableDialog: SearchableDialog
+    var searchListItems: MutableList<SearchListItem> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        for (i in 0..9) {
+            val searchListItem = SearchListItem(i, "Country $i")
+            searchListItems.add(searchListItem)
+        }
+        val countryNameTextView = findViewById<TextView>(R.id.countryNameTextView)
+        val countryCodeTextView = findViewById<TextView>(R.id.countryCodeTextView)
+        searchableDialog = SearchableDialog(this, searchListItems, getString(R.string.country))
+        searchableDialog.setOnItemSelected { _, searchListItem ->
+            countryCodeTextView.text = searchListItem.getId().toString()
+            countryNameTextView.text = searchListItem.getTitle()
+        }
+        val countryTextInputEditText = findViewById<TextInputEditText>(R.id.countryTextInputEditText)
+        countryTextInputEditText.setOnClickListener { searchableDialog.show() }
+
+
+
+    }
+
+
+}
